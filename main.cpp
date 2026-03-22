7997b73619da686a38e30fd0abfc1d0335b7fc9f
mr90733 <mr90733@mail.harpercollege.edu>
2026-03-21 21:40:04 -0500

    Meet in the middle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 #include "src/meet.hpp"
 
-int main() {
-  std::cout << meet() << std::endl;
+using namespace std;
+
+int main()
+
+{
+    cout << "Enter the filename: ";
+
+    string filename; cin >> filename;
+
+    DataSet ds(filename);
+
+    cout << "The median of the dataset is " << median(ds) << endl;
 }
--- src/meet.hpp
+++ src/meet.hpp
@@ -1,3 +1,56 @@
+#pragma once
+
+#include <iostream>
+
+#include <fstream>
+
+#include <sstream>
+
 #include <string>
 
-std::string meet();
+#include <cmath>
+
+using namespace std;
+
+class DataSet
+
+{
+
+private:
+
+    int* data;
+
+    size_t size;
+
+    double med;
+
+public:
+
+    explicit DataSet(const string& filename) : data(nullptr), size(0), med(0.0)
+
+    {
+        ifstream file(filename);
+
+        if (!file) { cerr << "Not valid\n"; return; }
+
+        string line; getline(file, line);
+
+        stringstream ss(line); string token;
+
+        while (getline(ss, token, ',')) size++;
+
+        data = new int[size];
+
+        ss.clear(); ss.str(line);
+
+        for (size_t i = 0; i < size; ++i) { getline(ss, token, ','); data[i] = stoi(token); }
+
+        if (size % 2 == 1) med = data[size/2];
+
+        else med = (data[size/2 -1] + data[size/2]) / 2.0;
+    }
+
+    ~DataSet() { delete[] data; }
+
+    friend double median(const DataSet& ds) { return ds.med; }
+};
